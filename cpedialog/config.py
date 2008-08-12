093f2b59690d124352e41a6d5121da35556877e0
cpedia <cpedia@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2008-08-12 18:11:16 +0000

    add google ajax feed feature.

--- cpedialog/config.py
+++ cpedialog/config.py
@@ -7,6 +7,9 @@ blog = {
     "description": "CPEDIA's Blog powered by Google AppEngine.",
     "root_url": "http://blog.cpedia.com",
     "cache_time": 0,      # You can override this default for each page through a handler's call to view.ViewPage(cache_time=...)
+
+    #apply google ajax feed key for your domain from: http://code.google.com/apis/ajaxfeeds/signup.html
+    "google_ajax_feed_key":"ABQIAAAAOY_c0tDeN-DKUM-NTZldZhQG0TqTy2vJ9mpRzeM1HVuOe9SdDRSieJccw-q7dBZF5aGxGJ-oZDyf5Q"
 }
 
 album ={
