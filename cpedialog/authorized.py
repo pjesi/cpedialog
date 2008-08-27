8d2a0f5aa1251e17207b61c9dfa08cdb5e8e2a15
cpedia <cpedia@a1bfd6c9-7850-0410-82cb-e7152bfa5976>
2008-08-27 17:52:09 +0000

    add Apache License explanation to the code.

--- cpedialog/authorized.py
+++ cpedialog/authorized.py
@@ -1,3 +1,20 @@
+# !/usr/bin/env python
+#
+# Copyright 2008 CPedia.com.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 __author__ = 'Ping Chen'
 
 
@@ -10,7 +27,9 @@ import urllib
 from model import AuthSubStoredToken
 
 def role(role):
-    """A decorator to enforce user roles, currently 'user' (logged in) and 'admin'.
+    """This method refer to the Bloog (http://bloog.appspot.com).
+
+    A decorator to enforce user roles, currently 'user' (logged in) and 'admin'.
 
     To use it, decorate your handler methods like this:
 
